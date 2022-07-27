92965cc3cf8a8671f8ebadd1456809ffab5a028f
derekmeyer37 <derekmeyer37@gmail.com>
2022-07-26 18:03:45 -0600

    Items for July 27 meeting

---
+++ R/epiworldR-package.R
@@ -0,0 +1,15 @@
+#' first_function()
+#' @param x This is a number
+#' @param y This is another number
+#' @param z This is the last number 
+#' @return The sum of \code{x}, \code{y}, and \code{z}.
+#' @examples 
+#' first_function(1, 1, 3)
+#' first_function(13, 2, 400)
+
+first_function <- function(x,y,z) {
+  x + y + z
+}
+
+?first_function
+
