912a688ec787eb04cf35b37ca1454b719e689825
derekmeyer37 <derekmeyer37@gmail.com>
2022-11-21 14:23:25 -0700

    Documentation Update

--- R/model-methods.R
+++ R/model-methods.R
@@ -1,5 +1,11 @@
 #' Common methods for predefined models of Epiworld
-#' @param m,days,seed,n,k,d,p to be explained
+#' @param days Number of days.
+#' @param seed Seed to set for initializing random number generator.
+#' @param m Model object.
+#' @param n Number of individuals in the population.
+#' @param k Number of ties in the small world network.
+#' @param d Logical scalar. Whether the graph is directed or not.
+#' @param p Probability of rewiring.
 #' @export
 #' @name epiworld-methods
 init <- function(m, days, seed) UseMethod("init")
