cc673d1c8f9310b3c54c831563adf200c3181480
George G. Vega Yon <g.vegayon@gmail.com>
2024-11-20 11:27:07 -0700

    Lfmcmc rand engine (#51)
    * Removing vscode cached
    
    * Updating news file and correcting how seeds are set
    
    * Adding another set of tests
    
    * Modified vignette with better example
    
    * Remove unneeded code in kernelfun in lfmcmc vignette
    
    * Move test from test-lfmcmc-other-models.R to test-lfmcmc.R
    
    * Add Andrew Pulsipher as co-author
    
    * Fix LFMCMC unit tests
    
    * Fix errors in examples in LFMCMC.R
    
    * Document data type coercion in lfmcmc.R
    
    * Add test of type coercion to test-lfmcmc.R
    
    * Fix type coercion tests in test-lfmcmc.R to use correct values
    
    ---------
    
    Co-authored-by: Andrew Pulsipher <a.pulsipher@utah.edu>

--- inst/tinytest/test-lfmcmc.R
+++ inst/tinytest/test-lfmcmc.R
@@ -5,13 +5,15 @@ model_seed <- 122
 
 # Create and run SIR Model for LFMCMC simulation -------------------------------
 model_sir <- ModelSIR(name = "COVID-19", prevalence = .1, 
-                      transmission_rate = .9, recovery_rate = .3)
+                      transmission_rate = .3, recovery_rate = .3)
 agents_smallworld(model_sir, n = 1000, k = 5, d = FALSE, p = 0.01)
 verbose_off(model_sir)
 run(model_sir, ndays = 50, seed = model_seed)
 
+# Check init of LFMCMC model without epiworld model ----------------------------
+expect_silent(lfmcmc_nomodel <- LFMCMC())
+
 # Check bad init of LFMCMC model -----------------------------------------------
-expect_error(lfmcmc_bad <- LFMCMC(), 'argument "model" is missing')
 expect_error(lfmcmc_bad <- LFMCMC(c("not_a_model")), "model should be of class 'epiworld_model'")
 
 # Create LFMCMC model ----------------------------------------------------------
@@ -22,7 +24,7 @@ expect_inherits(lfmcmc_model, "epiworld_lfmcmc")
 expect_length(class(lfmcmc_model), 1)
 
 # Extract observed data from the model
-obs_data <- unname(as.integer(get_today_total(model_sir)))
+obs_data <- get_today_total(model_sir)
 
 expect_silent(set_observed_data(lfmcmc_model, obs_data))
 
@@ -31,20 +33,19 @@ simfun <- function(params) {
   set_param(model_sir, "Recovery rate", params[1])
   set_param(model_sir, "Transmission rate", params[2])
   run(model_sir, ndays = 50)
-  res <- unname(as.integer(get_today_total(model_sir)))
+  res <- get_today_total(model_sir)
   return(res)
 }
 
 sumfun <- function(dat) { return(dat) }
 
 propfun <- function(params_prev) {
-  res <- params_prev + rnorm(length(params_prev), )
+  res <- plogis(qlogis(params_prev) + rnorm(length(params_prev)))
   return(res)
 }
 
 kernelfun <- function(stats_now, stats_obs, epsilon) {
-  ans <- sum(mapply(function(v1, v2) (v1 - v2)^2, stats_obs, stats_now))
-  return(ifelse(sqrt(ans) < epsilon, 1.0, 0.0))
+  dnorm(sqrt(sum((stats_now - stats_obs)^2)))
 }
 
 # Check adding functions to LFMCMC
@@ -55,9 +56,9 @@ expect_silent(set_kernel_fun(lfmcmc_model, kernelfun))
 
 # Run LFMCMC simulation --------------------------------------------------------
 # Initial parameters
-par0 <- as.double(c(0.1, 0.5))
+par0 <- c(0.1, 0.5)
 n_samp <- 2000
-epsil <- as.double(1.0)
+epsil <- 1.0
 
 expect_silent(run_lfmcmc(
   lfmcmc = lfmcmc_model,
@@ -72,8 +73,8 @@ expect_silent(set_par_names(lfmcmc_model, c("Immune recovery", "Infectiousness")
 
 expect_stdout(print(lfmcmc_model))
 
-expect_equal(get_stats_mean(lfmcmc_model), c(4.45, 2.6135, 992.4365))
-expect_equal(get_params_mean(lfmcmc_model), c(11.58421, 18.96851), tolerance = 0.00001)
+expect_equal(get_stats_mean(lfmcmc_model), c(284.7140, 0.8485, 713.9375))
+expect_equal(get_params_mean(lfmcmc_model), c(0.3132901, 0.2782186), tolerance = 0.00001)
 
 # Check LFMCMC using factory functions -----------------------------------------
 expect_silent(use_proposal_norm_reflective(lfmcmc_model))
@@ -84,6 +85,22 @@ expect_silent(run_lfmcmc(
   params_init_ = par0,
   n_samples_ = n_samp,
   epsilon_ = epsil,
+  seed = model_seed
+))
+
+# Check LFMCMC type coercion of parameters and observed data -------------------
+obs_data_int <- as.integer(obs_data)
+expect_silent(set_observed_data(lfmcmc_model, obs_data_int))
+
+par0_int <- as.integer(c(1, 5))
+n_samp_double <- as.double(2000.0)
+epsil_int <- as.integer(1)
+
+expect_silent(run_lfmcmc(
+  lfmcmc = lfmcmc_model,
+  params_init_ = par0_int,
+  n_samples_ = n_samp_double,
+  epsilon_ = epsil_int,
   seed = model_seed
 ))
 
@@ -120,3 +137,60 @@ expect_error(run_lfmcmc(
 ))
 
 expect_error(run_lfmcmc(lfmcmc = lfmcmc_model))
+
+# Check running LFMCMC without epiworld model ----------------------------------
+model_seed <- 4222
+set.seed(model_seed)
+Y <- rnorm(2000, mean = -5, sd = 2.5)
+
+# Define LFMCMC functions
+simfun <- function(par) {
+  rnorm(2000, mean = par[1], sd = par[2])
+}
+
+sumfun <- function(x) {
+  c(mean(x), sd(x))
+}
+
+propfun <- function(par) {
+  
+  par_new <- par + rnorm(2, sd = 0.1)
+
+  # Reflecting par2
+  if (par_new[2] < 0) {
+    par_new[2] <- par[2] - (par_new[2] - par[2])
+  }
+
+  return(par_new)
+}
+
+kernelfun <- function(stats_now, stats_obs, epsilon) {
+
+  dnorm(sqrt(sum((stats_obs - stats_now)^2)))
+  
+}
+
+# Setup LFMCMC
+lfmcmc_model <- LFMCMC() |>
+  set_simulation_fun(simfun) |>
+  set_summary_fun(sumfun) |>
+  set_proposal_fun(propfun) |>
+  set_kernel_fun(kernelfun) |>
+  set_observed_data(Y)
+
+# Run LFMCMC
+x <- run_lfmcmc(
+  lfmcmc = lfmcmc_model,
+  params_init_ = c(0, 1),
+  n_samples_ = 3000,
+  epsilon_ = 1.0,
+  seed = model_seed
+)
+
+x_means <- get_params_mean(x)
+
+expect_equivalent(
+  x_means,
+  c(-5, 2.5),
+  tolerance = 0.1
+)
--- src/lfmcmc.cpp
+++ src/lfmcmc.cpp
@@ -9,7 +9,7 @@
 
 using namespace epiworld;
 
-#define TData_default std::vector< int >
+#define TData_default std::vector< double >
 
 #define WrapLFMCMC(a) \
     cpp11::external_pointer<LFMCMC<TData_default>> (a)
@@ -29,8 +29,13 @@ SEXP LFMCMC_cpp(
         new LFMCMC<TData_default>()
     );
 
-    cpp11::external_pointer<Model<int>> modelptr(model);
-    lfmcmc_ptr->set_rand_engine(modelptr->get_rand_endgine());
+    if (Rf_inherits(model, "epiworld_model")) {
+        cpp11::external_pointer<Model<int>> modelptr(model);
+        lfmcmc_ptr->set_rand_engine(modelptr->get_rand_endgine());
+    } else {
+        auto new_ptr = std::make_shared<std::mt19937>(std::mt19937());
+        lfmcmc_ptr->set_rand_engine(new_ptr);
+    }
 
     return lfmcmc_ptr;
 }
@@ -42,9 +47,9 @@ SEXP LFMCMC_cpp(
 [[cpp11::register]]
 SEXP run_lfmcmc_cpp(
     SEXP lfmcmc,
-    std::vector<epiworld_double> params_init_,
+    std::vector<double> params_init_,
     size_t n_samples_,
-    epiworld_double epsilon_,
+    double epsilon_,
     int seed
 ) {
     WrapLFMCMC(lfmcmc_ptr)(lfmcmc);
@@ -60,7 +65,7 @@ SEXP run_lfmcmc_cpp(
 [[cpp11::register]]
 SEXP set_observed_data_cpp(
     SEXP lfmcmc,
-    std::vector< int > observed_data_
+    std::vector< double > observed_data_
 ) {
     WrapLFMCMC(lfmcmc_ptr)(lfmcmc);
     lfmcmc_ptr->set_observed_data(observed_data_);
@@ -105,7 +110,7 @@ SEXP use_proposal_norm_reflective_cpp(
     SEXP lfmcmc
 ) {
     WrapLFMCMC(lfmcmc_ptr)(lfmcmc);
-    lfmcmc_ptr->set_proposal_fun(make_proposal_norm_reflective<std::vector<int>>(.5, 0, 1));
+    lfmcmc_ptr->set_proposal_fun(make_proposal_norm_reflective<std::vector<double>>(.5, 0, 1));
     return lfmcmc;
 }
 
@@ -123,7 +128,7 @@ SEXP set_simulation_fun_cpp(
         auto params_doubles = cpp11::doubles(params);
 
         return cpp11::as_cpp<TData_default>(
-            cpp11::integers(fun(params_doubles))
+            cpp11::doubles(fun(params_doubles))
             );
     };
 
@@ -146,8 +151,8 @@ SEXP set_summary_fun_cpp(
         LFMCMC<TData_default>*
         ) -> void {
 
-        auto dat_int = cpp11::integers(dat);
-        auto res_tmp = cpp11::integers(fun(dat_int));
+        auto dat_int = cpp11::doubles(dat);
+        auto res_tmp = cpp11::doubles(fun(dat_int));
 
         if (res.size() == 0u)
             res.resize(res_tmp.size());
@@ -198,7 +203,7 @@ SEXP use_kernel_fun_gaussian_cpp(
     SEXP lfmcmc
 ) {
     WrapLFMCMC(lfmcmc_ptr)(lfmcmc);
-    lfmcmc_ptr->set_kernel_fun(kernel_fun_gaussian<std::vector<int>>);
+    lfmcmc_ptr->set_kernel_fun(kernel_fun_gaussian<std::vector<double>>);
     return lfmcmc;
 }
 
