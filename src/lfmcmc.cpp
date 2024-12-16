4161faa0d49ab9366f535f7f817f005645e0d109
George G. Vega Yon <g.vegayon@gmail.com>
2024-12-16 08:31:50 -0700

    Passing LFMCMC object to functions and improve docs (#64)
    * Adding missing element [skip ci]
    
    * Adding object to summary function
    
    * Adding class attribute
    
    * Renaming parameters and adding more info in the manual
    
    * Rename model param in example lfmcmc functions
    
    * Add tests to verify lfmcmc_obj in lfmcmc functions
    
    ---------
    
    Co-authored-by: Andrew Pulsipher <a.pulsipher@utah.edu>

--- src/lfmcmc.cpp
+++ src/lfmcmc.cpp
@@ -14,6 +14,13 @@ using namespace epiworld;
 #define WrapLFMCMC(a) \
     cpp11::external_pointer<LFMCMC<TData_default>> (a)
 
+inline cpp11::sexp lfmcmc_as_sexp(LFMCMC<TData_default> * lfmcmc) {
+    WrapLFMCMC(lfmcmc_ptr)(lfmcmc, false);
+    cpp11::sexp lfmcmc_ptr_s(lfmcmc_ptr);
+    lfmcmc_ptr_s.attr("class") = "epiworld_lfmcmc";
+    return lfmcmc_ptr_s;
+}
+
 // LFMCMC definitions:
 // https://github.com/UofUEpiBio/epiworld/tree/master/include/epiworld/math/lfmcmc
 
@@ -81,12 +88,12 @@ SEXP set_proposal_fun_cpp(
     LFMCMCProposalFun<TData_default> fun_call = [fun](
         std::vector< epiworld_double >& new_params,
         const std::vector< epiworld_double >& old_params,
-        LFMCMC<TData_default>*
+        LFMCMC<TData_default> * lfmcmc_obj
         ) -> void {
 
         auto params_doubles = cpp11::doubles(old_params);
-
-        auto res_tmp = cpp11::doubles(fun(params_doubles));
+        auto lfmcmc_ptr = lfmcmc_as_sexp(lfmcmc_obj);
+        auto res_tmp = cpp11::doubles(fun(params_doubles, lfmcmc_ptr));
 
         std::copy(
             res_tmp.begin(),
@@ -122,13 +129,13 @@ SEXP set_simulation_fun_cpp(
 
     LFMCMCSimFun<TData_default> fun_call = [fun](
         const std::vector<epiworld_double>& params,
-        LFMCMC<TData_default>*
+        LFMCMC<TData_default> * lfmcmc_obj
         ) -> TData_default {
 
         auto params_doubles = cpp11::doubles(params);
-
+        auto lfmcmc_ptr = lfmcmc_as_sexp(lfmcmc_obj);
         return cpp11::as_cpp<TData_default>(
-            cpp11::doubles(fun(params_doubles))
+            cpp11::doubles(fun(params_doubles, lfmcmc_ptr))
             );
     };
 
@@ -148,11 +155,13 @@ SEXP set_summary_fun_cpp(
     LFMCMCSummaryFun<TData_default> fun_call = [fun](
         std::vector< epiworld_double >& res,
         const TData_default& dat,
-        LFMCMC<TData_default>*
+        LFMCMC<TData_default>* lfmcmc_obj
         ) -> void {
 
         auto dat_int = cpp11::doubles(dat);
-        auto res_tmp = cpp11::doubles(fun(dat_int));
+        auto lfmcmc_ptr = lfmcmc_as_sexp(lfmcmc_obj);
+        auto res_tmp = cpp11::doubles(fun(dat_int, lfmcmc_ptr));
+
 
         if (res.size() == 0u)
             res.resize(res_tmp.size());
@@ -179,14 +188,16 @@ SEXP set_kernel_fun_cpp(
         const std::vector< epiworld_double >& simulated_stats,
         const std::vector< epiworld_double >& observed_stats,
         epiworld_double epsilon,
-        LFMCMC<TData_default>*
+        LFMCMC<TData_default> * lfmcmc_obj
         ) -> epiworld_double {
 
         auto sim_stats_doubles = cpp11::doubles(simulated_stats);
         auto obs_stats_doubles = cpp11::doubles(observed_stats);
 
+        auto lfmcmc_ptr = lfmcmc_as_sexp(lfmcmc_obj);
+
         return cpp11::as_cpp<epiworld_double>(
-            fun(sim_stats_doubles, obs_stats_doubles, epsilon)
+            fun(sim_stats_doubles, obs_stats_doubles, epsilon, lfmcmc_ptr)
             );
     };
 
