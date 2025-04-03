d324df9f8e978629610150a2a9660aafd9f7575d
Andrew Pulsipher <a.pulsipher@utah.edu>
2025-04-03 16:33:09 -0600

    Update param names in draw_mermaid() and add tests

--- R/model-methods.R
+++ R/model-methods.R
@@ -411,16 +411,16 @@ clone_model <- function(model) {
 
 #' @rdname epiworld-methods
 #' @export
-#' @param fn_output String. Path to the output file.
+#' @param output_file String. Path to the output file.
 #' @param allow_self_transitions Logical. Whether to allow self-transitions.
 #' @details `draw_mermaid` generates a mermaid diagram of the model. The
 #' diagram is saved in the specified output file (or printed to the standard
 #' output if the filename is empty).
 #' @return
 #' - `draw_mermaid` returns the model that was drawn.
-draw_mermaid <- function(model, fn_output = "", allow_self_transitions = FALSE) {
+draw_mermaid <- function(model, output_file = "", allow_self_transitions = FALSE) {
   stopifnot_model(model)
-  stopifnot_string(fn_output)
+  stopifnot_string(output_file)
   stopifnot_bool(allow_self_transitions)
-  invisible(draw_mermaid_cpp(model, fn_output, allow_self_transitions))
+  invisible(draw_mermaid_cpp(model, output_file, allow_self_transitions))
 }
---
+++ inst/tinytest/test-model-methods.R
@@ -0,0 +1,29 @@
+# Test just this file: tinytest::run_test_file("inst/tinytest/test-model-methods.R")
+
+model <- ModelSIRCONN(
+  name = "A Virus",
+  n = 10000,
+  prevalence = .01,
+  contact_rate = 4.0,
+  transmission_rate = .5,
+  recovery_rate = 1.0/7.0
+)
+
+verbose_off(model)
+run(model, ndays = 50, seed = 1912)
+
+# Check draw_mermaid succeeds with valid inputs --------------------------------
+expect_stdout(draw_mermaid(model))
+expect_stdout(draw_mermaid(model, allow_self_transitions = TRUE))
+expect_silent(draw_mermaid(model, output_file = "mermaid_diagram.txt"))
+expect_true(file.exists("mermaid_diagram.txt"))
+file.remove("mermaid_diagram.txt")
+expect_false(file.exists("mermaid_diagram.txt"))
+
+# Check functions fail with invalid inputs -------------------------------------
+
+# Check draw_mermaid function
+expect_error(draw_mermaid(NULL), "must be of class 'epiworld_model'.")
+expect_error(draw_mermaid(model, output_file = NULL), "must be a string")
+expect_error(draw_mermaid(model, allow_self_transitions = NA), "must not be NA")
+expect_error(draw_mermaid(model, allow_self_transitions = "NOT_BOOL"), "must be a boolean")
